cab884b327d964543e2638a64844954d2c6429d0
Khant Kyaw <khantkankei@gmail.com>
2024-02-13 15:53:25 +0630

    update include

--- index.js
+++ index.js
@@ -56,7 +56,9 @@ app.get('/posts/:id', async (req, res) => {
 		const response = await axios.get(`${JSONURL}/${id}`);
 		const result = await client.set(
 			`post-${id}`,
-			JSON.stringify(response.data)
+			JSON.stringify(response.data),
+			'EX',
+			10
 		);
 
 		res.json({
@@ -81,6 +83,60 @@ app.get('/', async (req, res) => {
 			isSuccess: true,
 			data: value,
 		});
+	} catch (error) {
+		res.status(500).json({
+			isSuccess: false,
+			message: 'Internal Server Error',
+			error,
+		});
+	}
+});
+
+app.put('/posts/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const { updatedValue } = req.body;
+
+		// Check if the key already exists in Redis
+		const cachedPost = await client.get(`post-${id}`);
+
+		if (cachedPost) {
+			// Update the existing data in Redis
+			const updatedData = JSON.parse(cachedPost);
+			updatedData.updatedValue = updatedValue; // Modify as needed
+
+			await client.set(
+				`post-${id}`,
+				JSON.stringify(updatedData),
+				'EX',
+				10
+			);
+
+			res.json({
+				isSuccess: true,
+				data: updatedData,
+				message: 'Data updated successfully.',
+			});
+		} else {
+			// Perform the update operation (e.g., using axios)
+			const updateResponse = await axios.put(`${JSONURL}/${id}`, {
+				updatedValue,
+			});
+
+			// Update the cached data in Redis
+			await client.set(
+				`post-${id}`,
+				JSON.stringify(updateResponse.data),
+				'EX',
+				10
+			);
+
+			res.json({
+				isSuccess: true,
+				data: updateResponse.data,
+				message: 'Data created and cached successfully.',
+			});
+		}
 	} catch (error) {
 		res.status(500).json({
 			isSuccess: false,
